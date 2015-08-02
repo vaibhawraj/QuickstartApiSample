0e50796b21251dc7fa9b6a70690e67b120fd479d
Vaibhaw <vaibhawraj@yahoo.in>
2015-08-02 18:03:23 +0530

    Adding Initial Code

---
+++ src/com/example/samples/QuickstartApiSample.java
@@ -0,0 +1,23 @@
+/*
+ * Name:				QuickstartApiSample
+ * Created On:			2 Aug 2014
+ * Author:				Vaibhaw Raj (vaibhaw.raj@comprotechnologies.com)
+ * Description:			Quick Example from SOAP API Developer's Guide. It is a
+ *						sample Java client application that uses the WSC SOAP
+ *						client.
+ * Change Log History :
+ * |--------------------------------------------------------------------------|
+ * | Version | Changes By   | Date       | Descripton                         |
+ * |--------------------------------------------------------------------------|
+ * | 0.1     | Vaibhaw      | 02-08-2015 | Initial version of Class           |
+ * |         |              |            |                                    |
+ * |--------------------------------------------------------------------------|
+ *
+ */
+
+package com.example.samples;
+
+import java.io.BufferedReader;
+public class QuickstartApiSample {
+
+}
